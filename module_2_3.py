fb9f7e8fdc524ba9a63a69a1317f27110dee0bdc
Dinapython <gafurovamadi@gmail.com>
2024-11-05 10:35:03 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,12 @@
 my_list =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-postive = []
 index = 0
+
 while index < len(my_list):
+
     if my_list[index] < 0:
        break
-    postive.append(my_list[index])
+
+    if my_list[index] > 0:
+       print(my_list[index])
+
     index += 1
-print(postive)
